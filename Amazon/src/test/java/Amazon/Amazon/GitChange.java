9366a4622d14797241d55161894149612d095675
krakh <krakh@WINDOWS-N0Q5TOC>
2023-04-06 20:27:11 -0400

    Second commit

---
+++ Amazon/src/test/java/Amazon/Amazon/GitChange.java
@@ -0,0 +1,10 @@
+package Amazon.Amazon;
+
+public class GitChange {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a new change");
+	}
+
+}
